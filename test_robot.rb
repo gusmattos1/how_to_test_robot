fc5120a542c7afea5e19ebd05a42f142217b0b50
gusmattos1 <gusmattos1@gmail.com>
2018-04-30 21:34:19 -0400

    assignment done

--- test_robot.rb
+++ test_robot.rb
@@ -5,75 +5,115 @@ require './robot.rb'
 class TestRobot < MiniTest::Test
 
   def test_that_foreign_robot_needing_repairs_sent_to_station_1
-    skip
-    # arrange
+
+        # arrange
+
+    foreign_robot = Robot.new
+
+    foreign_robot.foreign_model = true
+    foreign_robot.needs_repairs = true
 
     # act
 
+    expected_result = 1
+
+    actual_result = foreign_robot.station
     # assert
+
+    assert_equal(expected_result, actual_result)
   end
 
   def test_that_vintage_robot_needing_repairs_sent_to_station_2
-    skip
+
     # arrange
 
-    # act
+     vintage_robot = Robot.new
+     vintage_robot.vintage_model=(true)
+     vintage_robot.needs_repairs=(true)
+      # act
 
-    # assert
+     expected_result = 2
+     actual_result = vintage_robot.station
+      # assert
+     assert_equal(expected_result, actual_result)
   end
 
   def test_that_standard_robot_needing_repairs_sent_to_station_3
-    skip
+
     # arrange
 
-    # act
+    standard_robot = Robot.new
+    standard_robot.needs_repairs=(true)
+     # act
 
-    # assert
+    expected_result = 3
+    actual_result = standard_robot.station
+     # assert
+    assert_equal(expected_result, actual_result)
   end
 
   def test_that_robot_in_good_condition_sent_to_station_4
-    skip
+
     # arrange
+        good_robot = Robot.new
+         # act
 
-    # act
-
-    # assert
+        expected_result = 4
+        actual_result = good_robot.station
+         # assert
+        assert_equal(expected_result, actual_result)
   end
 
   def test_prioritize_tasks_with_empty_todo_list_returns_negative_one
-    skip
     # arrange
 
-    # act
+        robot = Robot.new
+         # act
 
-    # assert
+        expected_result = -1
+        actual_result = robot.prioritize_tasks
+         # assert
+        assert_equal(expected_result, actual_result)
   end
 
   def test_prioritize_tasks_with_todos_returns_max_todo_value
-    skip
     # arrange
 
-    # act
+       robot = Robot.new
+       robot.todos = ['kill everybody', 'refil oil']
+        # act
 
-    # assert
-  end
+       expected_result = 'kill everybody'
+       actual_result = robot.prioritize_tasks
+        # assert
+       assert_equal(expected_result, actual_result)
+
+     end
 
   def test_workday_on_day_off_returns_false
-    skip
     # arrange
+    robot = Robot.new
+    robot.day_off=(12)
 
-    # act
+     # act
 
-    # assert
+    expected_result = false
+    actual_result = robot.workday?(12)
+     # assert
+    assert_equal(expected_result, actual_result)
   end
 
   def test_workday_not_day_off_returns_true
-    skip
     # arrange
 
-    # act
+        standard_issue = Robot.new
+        standard_issue.day_off=(12)
+         # act
 
-    # assert
+        expected_result = true
+        actual_result = standard_issue.workday?(13)
+         # assert
+        assert_equal(expected_result, actual_result)
   end
 
 end
